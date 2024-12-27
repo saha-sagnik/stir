5b9c6c440ad83ad4c1d0e1aef85fb2fe673f6408
Sagnik Saha <sagnik12347@gmail.com>
2024-12-28 01:51:31 +0530

    commit

--- src/index.js
+++ src/index.js
@@ -9,7 +9,7 @@ const PORT = process.env.PORT || 3000;
 
 // CORS Configuration
 const corsOptions = {
-  origin: ['http://127.0.0.1:5500', 'http://localhost:3000'], // Allow multiple origins
+  origin: ['hhttps://stir-nu.vercel.app', 'http://localhost:3000'], // Allow multiple origins
   optionsSuccessStatus: 200
 };
 app.use(cors(corsOptions));
